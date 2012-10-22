f9d230bfb1e6e8f2d63ae1c34c752ab417dfeea6
Prashanth <munichlinux@gmail.com>
2012-10-22 18:40:09 +0530

    Adding new files.

---
+++ dotemacs.d/elpa/svg-clock-0.4/svg-clock-autoloads.el
@@ -0,0 +1,27 @@
+;;; svg-clock-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (svg-clock) "svg-clock" "svg-clock.el" (20545 38874))
+;;; Generated autoloads from svg-clock.el
+
+(autoload 'svg-clock "svg-clock" "\
+Start/stop the svg clock.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("svg-clock-pkg.el") (20545 38874 564842))
+
+;;;***
+
+(provide 'svg-clock-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; svg-clock-autoloads.el ends here
